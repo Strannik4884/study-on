b0d201865603df2596e2a2b25d8a0f9da2adab57
Strannik4884 <strannik4884@mail.ru>
2021-03-05 00:37:36 +0300

    Updated course create page

--- src/Form/CourseType.php
+++ src/Form/CourseType.php
@@ -4,17 +4,48 @@ namespace App\Form;
 
 use App\Entity\Course;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class CourseType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('code')
-            ->add('name')
-            ->add('description')
+            ->add('code', TextType::class, [
+                'constraints' => [
+                    new Length([
+                        'max' => 255,
+                        'maxMessage' => 'Длина кода не должна превышать {{ limit }} символов'
+                    ]),
+                    new NotBlank([
+                        'message' => 'Код не может быть пустым'
+                    ])
+                ]
+            ])
+            ->add('name', TextType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Название не может быть пустым'
+                    ]),
+                    new Length([
+                        'max' => 255,
+                        'maxMessage' => 'Длина названия не должна превышать {{ limit }} символов'
+                    ])
+                ]
+            ])
+            ->add('description', TextareaType::class, [
+                'constraints' => [
+                    new Length([
+                        'max' => 1000,
+                        'maxMessage' => 'Длина описания не должна превышать {{ limit }} символов'
+                    ])
+                ]
+            ])
         ;
     }
 
